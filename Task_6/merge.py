faec5e8844962778bd7fcf535aeb74b5e1aaae24
Petros <p.tsitsekidis@gmail.com>
2024-02-14 23:20:04 +0200

    Completed task 6.

---
+++ Task_6/merge.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+buys = pd.read_csv('/home/petros/Desktop/mini_market/Task_5/customerBuys_Groups.csv')
+age = pd.read_csv('/home/petros/Desktop/mini_market/customerAge.csv')
+
+# Merge to include age
+merged_df = pd.merge(buys, age, on='CustomerID')
+
+# Assign a unique group ID for each transaction
+merged_df['GroupID'] = merged_df.groupby(['CustomerID', 'Date']).ngroup()
+
+# Create the binary matrix for products
+binary_matrix = pd.crosstab(index=merged_df['GroupID'], columns=merged_df['Product'])
+
+# Map 'Age' to 'GroupID'
+age_mapping = merged_df[['GroupID', 'Age']].drop_duplicates().set_index('GroupID')['Age']
+
+# Map Age
+binary_matrix['Age'] = binary_matrix.index.map(age_mapping)
+
+binary_matrix.to_csv('binary_matrix_with_age.csv', index=False)
